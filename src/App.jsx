f8d99ac9f45c582affb6300d9b210ff9f7354638
Aaron Hartono <aaronhartono28@gmail.com>
2024-11-16 10:35:50 +0700

    Fix navbar and add collorpalette

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/NavbarMUI";
 import "./App.css";
 
 function App() {
   return (
-    <>
-      <Navbar />
-    </>
+    <Navbar />
   );
 }
 
