260aceb5ae54de7e893cca1b370c3e140d0b49f6
Benedict Halim <benedicthalim1@gmail.com>
2024-11-17 10:32:08 +0700

    added hokusai layers & colour pallete

--- src/components/ColorPalette.jsx
+++ src/components/ColorPalette.jsx
@@ -1,52 +1,52 @@
-import { createTheme, ThemeProvider, alpha, getContrastRatio } from "@mui/material";
+import {
+  createTheme,
+  ThemeProvider,
+  alpha,
+  getContrastRatio,
+} from "@mui/material";
 
 export default function ColorPalette(props) {
-    let black_primary = '#0b1f1e';
-    let dark_primary = '#053331';
-    let primary = '#296460';
-    let secondary = '#bda26d';
-    // let white = '#d7d9d8';
-    let white = '#F6FAF9';
+  let black_primary = "#0E2E65";
+  let dark_primary = "#133e87";
+  let primary = "#608bc1";
+  let secondary = "#cbdceb";
+  let white = "#f3f3e0";
 
-    let theme = createTheme({});
-    theme = createTheme(theme, {
-        palette: {
-            dark_forest: theme.palette.augmentColor({
-                color: {
-                    main: black_primary,
-                },
-                name: 'dark_forest',
-            }),
-            forest: theme.palette.augmentColor({
-                color: {
-                    main: dark_primary,
-                },
-                name: 'forest',
-            }),
-            primary: theme.palette.augmentColor({
-                color: {
-                    main: primary,
-                },
-                name: 'primary',
-            }),
-            secondary: theme.palette.augmentColor({
-                color: {
-                    main: secondary,
-                    contrastText: white,
-                },
-                name: 'secondary',
-            }),
-            white: theme.palette.augmentColor({
-                color: {
-                    main: white,
-                },
-                name: 'white',
-            }),
+  let theme = createTheme({});
+  theme = createTheme(theme, {
+    palette: {
+      black_ocean: theme.palette.augmentColor({
+        color: {
+          main: black_primary,
         },
-    });
-    return (
-        <ThemeProvider theme={theme}>
-            {props.children}
-        </ThemeProvider>
-    );
-};
+        name: "black_ocean",
+      }),
+      dark_ocean: theme.palette.augmentColor({
+        color: {
+          main: dark_primary,
+        },
+        name: "dark_ocean",
+      }),
+      primary: theme.palette.augmentColor({
+        color: {
+          main: primary,
+        },
+        name: "primary",
+      }),
+      secondary: theme.palette.augmentColor({
+        color: {
+          main: secondary,
+          contrastText: white,
+        },
+        name: "secondary",
+      }),
+      white: theme.palette.augmentColor({
+        color: {
+          main: white,
+        },
+        name: "white",
+      }),
+    },
+  });
+  return <ThemeProvider theme={theme}>{props.children}</ThemeProvider>;
+}
